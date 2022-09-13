6509d342551978396a51d862244e5cf9b94c244d
unknown <jirrazu123@gmail.com>
2022-09-13 07:23:35 +0600

    2.1 Setup react-redux

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Counter from "./components/Counters";
 function App() {
   return (
     <div>
-      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
           Simple Counter Application
         </h1>
 
-        <div class="max-w-md mx-auto mt-10 space-y-5">
+        <div className="max-w-md mx-auto mt-10 space-y-5">
           <Counter />
           <Counter />
           <Counter />
