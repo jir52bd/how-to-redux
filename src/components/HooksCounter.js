ed3d01099ca5fe20f9f9cb1988255f53f4653139
unknown <jirrazu123@gmail.com>
2022-09-15 01:44:51 +0600

    2.6 Redux hook - useDispatch()

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,38 +1,35 @@
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from '../redux/counter/actions';
 
-function HooksCounter({id}) {
+function HooksCounter() {
   const count = useSelector((state) => state.value);
 
+  const dispatch = useDispatch();
+
+  const incrementHandler = (value) => {
+      dispatch(increment(value))
+  }
+
+  const decrementHandler = (value) => {
+    dispatch(decrement(value))
+  }
   return (
     <>
       <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-        <div className="text-2xl font-semibold">{ count } - {id}</div>
-        {/* <div className="flex space-x-3">
-          <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={increment}>
+        <div className="text-2xl font-semibold">{ count }</div>
+        <div className="flex space-x-3">
+          <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={() => incrementHandler(5)}>
             Increment
           </button>
-          <button className="bg-red-400 text-white px-3 py-2 rounded shadow" onClick={decrement}>
+          <button className="bg-red-400 text-white px-3 py-2 rounded shadow" onClick={() => decrementHandler(1)}>
             Decrement
           </button>
-        </div> */}
+        </div>
       </div>
     </>
   );
 }
 
-// const mapStateToProps = (state, ownProps) => {
-//   console.log(ownProps);
-//   return {
-//     count: state.value,
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     increment: (value) => dispatch(increment(value)),
-//     decrement: (value) => dispatch(decrement(value)),
-//   }
-// }
 
 export default HooksCounter
