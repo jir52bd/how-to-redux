56d845b4c2fb9994b0c0143c0baa3632f30dd18e
unknown <jirrazu123@gmail.com>
2022-09-15 05:04:43 +0600

    2.9 Redux Middleware concept

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,20 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 // import counterReducer from "./counter/counterReducer";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+//create our first midelware
+const myMiddleware = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Store: ${JSON.stringify(store.getState())}`);
+
+    const upCommingState = [action].reduce(rootReducer, store.getState());
+    
+
+    console.log(`Upcomming State: ${JSON.stringify(upCommingState)}`);
+
+    return next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(myMiddleware));
 
 export default store;
