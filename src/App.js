40024a53ae1ab540c0283b5cbd2ae7178787dd34
unknown <jirrazu123@gmail.com>
2022-09-15 06:47:06 +0600

    2.12 Crate a Shopping Cart - Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import { Provider } from "react-redux";
-import DynamicHooksCounter from "./components/DynamicHoocksCounter";
-import HooksCounter from "./components/HooksCounter";
-import VariableCounter from "./components/VariableCounter";
-import store from "./redux/store";
+
 
 function App() {
-  return (
-    <Provider store={store}>
-      <div>
-        <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-          <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-            Simple Counter Application
-          </h1>
-
-          <div className="max-w-md mx-auto mt-10 space-y-5">
-            <HooksCounter/>
-            <DynamicHooksCounter />
-            <VariableCounter />
-            <VariableCounter dynamic />
-          </div>
+  return <div>  
+          <p>Invoice App</p>
         </div>
-      </div>
-    </Provider>
-  );
 }
 
 export default App;
