aadfa7c0b70dcc4f45b0e2bbd7e334d13074fbf7
unknown <jirrazu123@gmail.com>
2022-09-15 07:59:24 +0600

    3.1 Template mustering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import Footer from "./components/Footer";
+import Head from "./components/Head";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div>  
-          <p>Invoice App</p>
-        </div>
+  return (
+    <div
+    class="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
+>
+
+    <Navbar />
+
+    <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+       
+        <Head />
+
+        <hr class="mt-4" />
+
+        <TodoList />
+
+        <hr class="mt-4" />
+
+        <Footer />
+        
+    </div>
+</div>
+  );
 }
 
 export default App;
