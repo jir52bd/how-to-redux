396aad430904ac39283f5dd18e6d6ccb0c0aad70
unknown <jirrazu123@gmail.com>
2022-09-22 05:50:32 +0600

    3.6 Todo List and Todo

--- src/redux/todos/actionTypes.js
+++ src/redux/todos/actionTypes.js
@@ -1,6 +1,6 @@
 export const ADDED = "todos/added";
 export const TOGGLED = "todos/toggle";
-export const COLORSELECTED = "todos/colorselectdd";
+export const COLORSELECTED = "todos/colorselected";
 export const DELETED = "todos/deleted";
 export const ALLCOMPLETED = "todos/allcompleted";
 export const CLEARCOMPLETED = "todo/clearcompleted";
